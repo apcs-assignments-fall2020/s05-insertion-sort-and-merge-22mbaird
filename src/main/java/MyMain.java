3c74c889964abf89cd76338535ef0049f67f6be3
22mbaird <22mbaird@athenian.org>
2021-02-11 14:38:58 +0000

    Attempt

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,16 +4,53 @@ public class MyMain {
 
     // Sorts the ArrayList using insertion sort
     public static ArrayList<Integer> insertionSort(ArrayList<Integer> list) {
-        // YOUR CODE HERE
-        return null;
+        
+        for (int i = 1; i < list.size(); i++){
+
+            
+            
+            for (int j =0; j<list.size();j++){
+                if ((list.get(i)<list.get(j+1) && (list.get(i) > list.get(j))){
+                    list.set(list.get(i),j);
+                }
+            }
+        }
+        return list;
+
+        
+        
     }
 
      // Merges two sorted arrays into one large combined
      // sorted array
      // You may assume arr1 and arr2 are the same length
     public static int[] merge(int[] arr1, int[] arr2) { 
-        // YOUR CODE HERE
-        return null;
+        int [] merged = new int[arr1.length + arr2.length];
+        int x = 0;
+        int y = 0;
+        int z = 0;
+        while (x < arr1.length && y < arr2.length){
+            if (arr1[x] < arr2[y]){
+                merged[z] = arr1[x];
+                x++;
+            }
+            else {
+                merged[z] = arr2[y];
+                y++;
+            }
+            z++;
+        }
+        while (x < arr1.length){
+            merged[z] = arr1[x];
+            x++;
+            z++;
+        }
+        while (y < arr2.length){
+            merged[z] = arr2[y];
+            y++;
+            z++;
+        }
+        return merged;
     }
 
     public static void main(String[] args) {
